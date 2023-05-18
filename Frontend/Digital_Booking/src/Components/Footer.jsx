afd859de2759b7727dccea56e0755e7203cf548b
yessenia <yejiga2006@gmail.com>
2023-05-17 20:32:47 -0500

    corregido el footer

--- Frontend/Digital_Booking/src/Components/Footer.jsx
+++ Frontend/Digital_Booking/src/Components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import logo from "../assets/logo1.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <img src="/Componentlogo3.png" id="logoFooter" alt="DB🎷" width={200} />
+      <div className="logoSlogan">
+        <img
+          src={logo}
+          id="logoFooter"
+          alt="DB🎷"
+          className="logo"
+          width={80}
+          height={50}
+        />
+        <p className="Pfooter">Lleva la musica con vos</p>
+      </div>
+      {/*<img src="logoconSlogan.png" id="logoFooter" alt="DB🎷" width={200} />*/}
       <p className="copyright">© 2023 Digital Booking</p>
     </footer>
   );
